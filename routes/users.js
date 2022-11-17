3e70c573d13a447747ce3a38284f3ab3e750f957
AZS-99 <adam.zachary.saher@gmail.com>
2022-11-17 01:56:10 -0500

    Tiral for user

--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,7 @@ router.post('/sign_up', async (req, res) => {
 
         delete user.password;
         req.session.user = user;
-        res.send(user)
+        res.send(res.session.user)
         // res.redirect('back');
     } catch (e) {
         console.log("ERROR!!!" + e)
